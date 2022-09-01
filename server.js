376ae0e3a668afd48be2f65db1e6ac1f8a43a881
web-shoharab-pk <shoharabpk.2019@gmail.com>
2022-09-01 19:36:56 +0600

    server route and db.json added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const data = require("./db.js");
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
